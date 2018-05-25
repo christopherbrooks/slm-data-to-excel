5d4db4393380e0768bad9fac429ff36da4df06f9
Ethan Brooks <ethanabrooks@gmail.com>
2018-05-25 13:02:40 -0400

    deleted white space and added argparse, a slightly more helpful argument parser than sys.argv

--- third_restored.py
+++ third_restored.py
@@ -1,14 +1,18 @@
 import os
 import csv
 import numpy as np
-import sys
+import argparse
 
-INPUT_DIR = sys.argv[1]
-OUTPUT_THIRD = os.path.join(INPUT_DIR, 'third_octave.csv')
-OUTPUT_OCTAVE = os.path.join(INPUT_DIR, 'octave.csv')
+parser = argparse.ArgumentParser()
+parser.add_argument('input_dir', help='where data comes from')
+parser.add_argument('output_dir', help='where new files go')
+args = parser.parse_args()
+
+OUTPUT_THIRD = os.path.join(args.output_dir, 'third_octave.csv')
+OUTPUT_OCTAVE = os.path.join(args.output_dir, 'octave.csv')
 
 line_info = [
-    (172, "Leq_third", 11 ), 
+    (172, "Leq_third", 11 ),
     (188, "Max_third", 19 ),
     (173, "Min_third", 17),
 ]
@@ -31,36 +35,36 @@ def octave_band(third_octave_values):
     return [dB_add(n) for n in range(0, len(third_octave_values), 3)]
 
 
-filename = os.listdir(INPUT_DIR)[0]
-with open(os.path.join(INPUT_DIR, filename), encoding="latin-1") as f:
+filename = os.listdir(args.input_dir)[0]
+with open(os.path.join(args.input_dir, filename), encoding="latin-1") as f:
     for line in f:
         if get_line_number(line) == 174:
             third_octave_band = ['','', "dBA"] + preprocess(line)[1:]
             break
-        
+
 with open(OUTPUT_THIRD, "w", newline="") as third_csv,\
         open(OUTPUT_OCTAVE, "w", newline="") as octave_csv:
     third_writer = csv.writer(third_csv)
     octave_writer = csv.writer(octave_csv)
 
-    octave_header = third_octave_band[:3] 
+    octave_header = third_octave_band[:3]
     octave_header += [third_octave_band[i] for i in range(4, len(third_octave_band), 3)]
 
     for third_octave_line, category, dBA_line in line_info:
         third_writer.writerow(third_octave_band)
         octave_writer.writerow(octave_header)
-        
-        for file_name in os.listdir(INPUT_DIR):
-            with open(os.path.join (INPUT_DIR,file_name), encoding="latin-1") as slmdl:
-   
+
+        for file_name in os.listdir(args.input_dir):
+            with open(os.path.join(args.input_dir,file_name), encoding="latin-1") as slmdl:
+
                 for line in slmdl:
                     line_number = get_line_number(line)
 
                     if line_number == third_octave_line:
                         third_octave_values = preprocess(line)[1:]
                     if line_number == dBA_line:
-                        dBA = preprocess(line.replace('Hz', ''))[1]        
-                
+                        dBA = preprocess(line.replace('Hz', ''))[1]
+
                 third_octave_row = [file_name, category, dBA] + third_octave_values
                 third_writer.writerow(third_octave_row)
                 octave_row = [file_name, category, dBA] + octave_band(third_octave_values)
